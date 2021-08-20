44c50863ab10e399c4aba37c965db56d13ea7956
Anatoly Kobzisty <anatolykobzisty@gmail.com>
2021-08-20 12:04:39 +0300

    Splitting Redux Store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,5 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import cartReducer from "../features/cart/cartSlice";
+import productsReducer from "../features/products/productsSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    cart: cartReducer,
+    products: productsReducer,
+  },
 });
